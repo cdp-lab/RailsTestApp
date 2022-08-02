5ea1f6307c53091e2bf1e61a007a54c67668e16b
Aleksandar Popovic <aleksandar.popovic@gmail.com>
2022-08-03 00:31:09 +0200

    Set for gem testing

--- config/initializers/bugsniffer_rails.rb
+++
@@ -1,6 +0,0 @@
-require_relative "../../lib/bugsniffer_rails/config"
-
-BugsnifferRails::Config.instance.setup do |config|
-  config.application_uid = "SDNSAwLaA23UFghiXcXDjCObpYprQ4ZNhMj5w_lRhgg"
-  config.application_secret = "Tsysai1QYtUNJYcbx0L_FTrdifSF_DShksejB11gtdU"
-end
