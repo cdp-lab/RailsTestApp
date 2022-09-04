64c4fb95c66a0dd2d8e54a57f4a88e988cbdfe02
Aleksandar Popovic <aleksandar.popovic@gmail.com>
2022-09-05 00:53:05 +0200

    Ref 0.2.3

--- config/initializers/bugsify.rb
+++ config/initializers/bugsify.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Bugsify.configure do |config|
-  config.api_key = "CGMmQ17VbcyhLLGGMTwOlDuSxJFTe4xRuheGfdbejiU"
-  config.api_secret = "n1BPyIn3zEV1R-OjuD-of8mwsuQ6MA_o4SR8BVjNHt0"
+  config.api_key = ""
+  config.api_secret = ""
 end
